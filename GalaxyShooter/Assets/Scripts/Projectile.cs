65eab5e8874b0372948dbc79496501fc02ab699d
tiakelly02 <72085203+tiakelly02@users.noreply.github.com>
2020-12-16 16:38:08 +0000

    Copy

---
+++ GalaxyShooter/Assets/Scripts/Projectile.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//This script is for the projectile/bullets to move
+
+public class Projectile : MonoBehaviour{
+
+    void Update(){
+
+       transform.Translate(new Vector3(0,3 * Time.deltaTime, 0));
+    }
+}
